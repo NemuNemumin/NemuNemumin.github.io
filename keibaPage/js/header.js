54c31317116aefb6a72e18e209743b8370cf398d
NemuNemumin <m0a20202af@edu.teu.ac.jp>
2022-06-11 12:18:58 +0900

    ヘッダーにロゴを追加

--- keibaPage/js/header.js
+++ keibaPage/js/header.js
@@ -1,9 +1,5 @@
 document.write('<div class="header">' +
-    '<nav class="navbar navbar-light bg-light">\n' +
-    '  <a class="navbar-brand">Navbar</a>\n' +
-    '  <form class="form-inline">\n' +
-    '    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">\n' +
-    '    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>\n' +
-    '  </form>\n' +
+    '<nav class="navbar navbar-light bg-logo my-1">\n' +
+    '  <img style="height: 50px;" src="img/Logo/maUmaTitleSqAlpha.png" alt="ロゴ">\n' +
     '</nav>' +
     '</div>');
