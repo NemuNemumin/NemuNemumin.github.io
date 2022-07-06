eb2772385f8122e77b14d0b58557219e22aa728d
NemuNemumin <m0a20202af@edu.teu.ac.jp>
2022-07-06 18:14:43 +0900

    jsonファイルの読込

--- maUma/shopData.js
+++ maUma/shopData.js
@@ -1,6 +1,15 @@
-var data
 $.getJSON("menu.json")
     .done(function (json) {
-        alert(data.name);
+        console.log(json);
+        let body = ";"
+        body += `<ul>`;
+        $.each(json, function(index, object){
+            body += `<li>${object.name}</li>`;
+            console.log(object.name);
+            console.log(JSON.stringify(object));
+        })
+        body += `</ul>`;
+        $("#shops").append(body);
+        // alert(json);
 
     });
