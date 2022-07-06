6c1817d297b9d41737bcafe88d02b22ded343771
NemuNemumin <m0a20202af@edu.teu.ac.jp>
2022-07-06 21:27:51 +0900

    jsonファイルの読込

--- maUma/shopDataNew.js
+++ maUma/shopDataNew.js
@@ -2,12 +2,12 @@ $.getJSON("menu.json")
     .done(function (json) {
         console.log(json);
 
-        let body = ";"
+        let body = ""
         // body += `<ul>`;
 
 
         $.each(json, function(index, object){
-            body += `<div class="flexStart bg-white roundedShopImg" id="shops">`
+            body += `<div class="flexStart bg-white roundedShopImg my-3" id="shops">`
             body += `<p id="name" class="text-left fs-l">${object.name}</p>`
             body += `<p id="menu" class = "text-left fs-m" >${object.menu}</p>`
 
