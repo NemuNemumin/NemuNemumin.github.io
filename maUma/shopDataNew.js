b2c9f1d64296fdda38d999b941ccf54a2321fc92
NemuNemumin <m0a20202af@edu.teu.ac.jp>
2022-07-08 09:59:32 +0900

    index.htmlを整理

--- maUma/shopDataNew.js
+++ maUma/shopDataNew.js
@@ -1,31 +1,20 @@
 $.getJSON("menu1.json")
+    //店情報の表示
     .done(function (json) {
         console.log(json);
-
         let body = ""
-        // body += `<ul>`;
-
-
         $.each(json, function (index, object) {
             body += `<div class = "flexStart bg-white roundedShopImg my-3" id="shops">`
 
             body += `<div class="shopImgBox">
                         <img alt="shop" class="shopImg" src="/maUma/sampleImg/${object.img}">
                      </div>`
-
             body += `<div class = "mx-2 mt-n1 position-relative" >`
             body += `<p id="name" class="text-left fs-l">${object.name}</p>`
             body += `<p id="menu" class = "text-left fs-m" >${object.menu}</p>`
             body += `<p id="place" class = "shopText fs-m">${object.place}</p>`
             body += `</div>`
-
             body += `</div>`
-            // $("#name").replaceWith(`<p id="name" class="text-left fs-l">${object.name}</p>`)
         })
-
-
-        //body += `</ul>`;
         $("#tags").append(body);
-        // alert(json);
-
     });
