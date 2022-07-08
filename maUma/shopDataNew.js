f1f9242065b7029f0f273ecce2ebe444e77736a7
NemuNemumin <m0a20202af@edu.teu.ac.jp>
2022-07-08 10:17:34 +0900

    jsコメント追加

--- maUma/shopDataNew.js
+++ maUma/shopDataNew.js
@@ -4,14 +4,13 @@ $.getJSON("menu1.json")
         let body =""
         $.each(json, function (index, object) {
             body += `<div class = "flexStart bg-white roundedShopImg my-3" id="shops">`
-
             body += `<div class="shopImgBox">
                         <img alt="shop" class="shopImg" src="/maUma/sampleImg/${object.img}">
                      </div>`
             body += `<div class = "mx-2 mt-n1 position-relative" >`
-            body += `<p id="name" class="text-left fs-l">${object.name}</p>`
-            body += `<p id="menu" class = "text-left fs-m" >${object.menu}</p>`
-            body += `<p id="place" class = "shopText fs-m">${object.place}</p>`
+            body += `<p id="name"  class = "text-left fs-l">${object.name}</p>`
+            body += `<p id="menu"  class = "text-left fs-m">${object.menu}</p>`
+            body += `<p id="place" class = "shopText  fs-m">${object.place}</p>`
             body += `</div>`
             body += `</div>`
         })
