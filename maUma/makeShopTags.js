f6780af2122b9c5aba5918a98961bbe0ea19d712
NemuNemumin <m0a20202af@edu.teu.ac.jp>
2022-07-14 20:36:07 +0900

    ボタン設置

--- maUma/makeShopTags.js
+++ maUma/makeShopTags.js
@@ -1,21 +1,24 @@
 $.getJSON("tokyoShop.json")
     //店情報の表示始め
     .done(function (json) {
-        let body =""
-            $.each(json, function (index, object) {
-                body += `<div class = "col-lg-6">`
-                body += `<div class = "flexStart bg-white roundedShopImg my-2" id="shops">`
-                body += `<div class="width-img">
+        let body = ""
+        $.each(json, function (index, object) {
+            body += `<div class = "col-lg-6">`
+            body += `<div class = "flexStart bg-white roundedShopImg my-2" id="shops">`
+            body += `<div class="width-img">
                          <img alt="shop" class="shopImg " src="/maUma/img/shopImg/${object.img}">
                          </div>`
-                body += `<div class = "mx-2 mt-n1 position-relative width-text">`
-                body += `<p id="name"  class = "fs-l">${object.name}</p>`
-                body += `<p id="menu"  class = "fs-m">${object.menu}</p>`
-                body += `<p id="place" class = "text-right shopText  fs-s" >${object.place}</p>`
-                body += `</div>`
-                body += `</div>`
-                body += `</div>`
-            })
-            $("#tags").append(body);//bodyをid=tagsの部分に追加
+            body += `<div class = "mx-2 mt-n1 position-relative width-text">`
+            body += `<p id="name"  class = "fs-l">${object.name}</p>`
+            body += `<p id="menu"  class = "fs-m">${object.menu}</p>`
+            body += `<p id="place" class = "text-right shopText  fs-s" >${object.place}</p>`
+            body += `</div>`
+            body += `</div>`
+            body += `</div>`
+
+        })
+        $("#tags").append(body);//bodyをid=tagsの部分に追加
     });
-    //店情報の表示終わり
+//店情報の表示終わり
+
+
---
+++ maUma/shopSelectOpen.js
@@ -0,0 +1,36 @@
+$(document).on("click", "#tokyo", function (){
+    console.log(".container clicked");
+});
+$(document).on("click", ".container", function (){
+    console.log(".container clicked");
+});
+$(document).on("click", "#", function (){
+    console.log(".container clicked");
+});
+$.getJSON("tokyoShop.json")
+    //店情報の表示始め
+    .done(function (json) {
+        let body = ""
+        $.each(json, function (index, object) {
+
+            if (object.tokyo === "1") {
+
+                body += `<div class = "col-lg-6">`
+                body += `<div class = "flexStart bg-white roundedShopImg my-2" id="shops">`
+                body += `<div class="width-img">
+                         <img alt="shop" class="shopImg " src="/maUma/img/shopImg/${object.img}">
+                         </div>`
+                body += `<div class = "mx-2 mt-n1 position-relative width-text">`
+                body += `<p id="name"  class = "fs-l">${object.name}</p>`
+                body += `<p id="menu"  class = "fs-m">${object.menu}</p>`
+                body += `<p id="place" class = "text-right shopText  fs-s" >${object.place}</p>`
+                body += `</div>`
+                body += `</div>`
+                body += `</div>`
+            }
+        })
+        $("#tags").append(body);//bodyをid=tagsの部分に追加
+    });
+//店情報の表示終わり
+
+
