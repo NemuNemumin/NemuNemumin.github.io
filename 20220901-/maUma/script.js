0fde6417ceee0355af4f3c91eecb6fcc0545bbb0
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2022-09-03 01:14:13 +0900

    入力フォームの値をjsに渡すところまで

--- 20220901-/maUma/script.js
+++ 20220901-/maUma/script.js
@@ -1,4 +1,14 @@
+//再現するボタンが押されると呼び出されるリプロダクション君//
 function reproduction() {
-    let corner1 = document.getElementById("corner1");//1コーナーの値を指定
+    let corner1 = document.getElementById("corner1");//1コーナーの値を取得
     console.log(corner1.value);
+    let corner2 = document.getElementById("corner2");//2コーナーの値を取得
+    console.log(corner2.value);
+    let corner3 = document.getElementById("corner3");//3コーナーの値を取得
+    console.log(corner3.value);
+    let corner4 = document.getElementById("corner4");//4コーナーの値を取得
+    console.log(corner4.value);
+    let raceResult = document.getElementById("raceResult");//最終順位の値を取得
+    console.log(raceResult.value);
+    //.valueは中身が値の目印。中身とかでもいいかも。//
 }
