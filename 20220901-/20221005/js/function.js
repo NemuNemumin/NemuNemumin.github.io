d31955eed96ce312d53bcff3ab30cb2bbb5a7dde
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2022-10-19 18:14:10 +0900

    関数とか

--- 20220901-/20221005/js/function.js
+++ 20220901-/20221005/js/function.js
@@ -1,6 +1,13 @@
 // 関数作成(まだ実行されない)
-function logHello(){
-  console.log("Hello");
+function logHello(text){
+    //``を使った文字列では、変数を埋め込める
+  console.log(`関数だったりするかも${text}`);
 }
-// 関数呼び出し
-logHello();
+function logabc(a,b,c){
+  console.log(`${a}${b}${c}`);
+}
+
+function logDefault(d="デフォルト"){
+  console.log(`${d}`);
+}
+
