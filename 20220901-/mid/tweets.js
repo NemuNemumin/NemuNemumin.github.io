2f8e8fbda8636d229c5ad239dda2dd49ca7dff7a
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2022-12-04 20:52:21 +0900

    中間課題

--- 20220901-/mid/tweets.js
+++ 20220901-/mid/tweets.js
@@ -1,10 +1,52 @@
-let answer = document.querySelector("#tweets");
+let tw = document.querySelector("#tweets");
 let html = "";
 
 let renderTweets = function (tweets){
   html += "<hr>"
-  html += `<img class="myAvatar" src="${tweets.avatar}"><b>${tweets.name}</b><br> ${tweets.message}<br><i>${tweets.tweetedAt}</i>`;
+  html += `<img class="myAvatar" src="${tweets.avatar}" alt=""><b>${tweets.name}</b><br> ${tweets.message}<br><i>${tweets.tweetedAt}</i>`;
+
 }
 tweets.forEach(renderTweets);
 html += "";
-answer.innerHTML = html;
+tw.innerHTML = html;
+
+function tweetsAll() {
+  html = "";
+  tweets.forEach(renderTweets);
+  html += "";
+  tw.innerHTML = html;
+}
+
+function tweetsSabro(){
+  html = "";
+  for (let i = 0 ; i < tweets.length; i++){
+    if (tweets[i].name === "三郎BOT"){
+      html += "<hr>"
+      html += `<img class="myAvatar" src="${tweets[i].avatar}" alt=""><b>${tweets[i].name}</b><br> ${tweets[i].message}<br><i>${tweets[i].tweetedAt}</i>`;
+    }
+  }
+tw.innerHTML = html;
+}
+
+function tweetsJiro(){
+  html = "";
+  for (let i = 0 ; i < tweets.length; i++){
+    if (tweets[i].name === "次郎"){
+      html += "<hr>"
+      html += `<img class="myAvatar" src="${tweets[i].avatar}" alt=""><b>${tweets[i].name}</b><br> ${tweets[i].message}<br><i>${tweets[i].tweetedAt}</i>`;
+    }
+  }
+  tw.innerHTML = html;
+}
+
+function tweetsTaro(){
+  html = "";
+  for (let i = 0 ; i < tweets.length; i++){
+    if (tweets[i].name === "太郎"){
+      html += "<hr>"
+      html += `<img class="myAvatar" src="${tweets[i].avatar}" alt=""><b>${tweets[i].name}</b><br> ${tweets[i].message}<br><i>${tweets[i].tweetedAt}</i>`;
+    }
+  }
+  tw.innerHTML = html;
+}
+
