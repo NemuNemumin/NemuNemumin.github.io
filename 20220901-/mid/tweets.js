c1d6e23a0c1251f962474f2aa60818179a4ececb
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2022-12-07 14:11:26 +0900

    関数をまとめた

--- 20220901-/mid/tweets.js
+++ 20220901-/mid/tweets.js
@@ -5,20 +5,19 @@ let renderTweets = function (tweets){
   html += `<img class="myAvatar" src="${tweets.avatar}" alt=""><b>${tweets.name}</b><br> ${tweets.message}<br><i>${tweets.tweetedAt}</i>`;
 }
 tweets.forEach(renderTweets);
-html += "";
 tw.innerHTML = html;
 
-function tweetsAll() {
-  html = "";
-  tweets.forEach(renderTweets);
-  tw.innerHTML = html;
-}
-function btnTweets(filter){
-  html = "";
-  for (let i = 0 ; i < tweets.length; i++){
-    if (tweets[i].name === filter){
-      html += "<hr>"
-      html += `<img class="myAvatar" src="${tweets[i].avatar}" alt=""><b>${tweets[i].name}</b><br> ${tweets[i].message}<br><i>${tweets[i].tweetedAt}</i>`;
+function btnTweets(filter = "ALL"){
+  if (filter === "ALL"){
+    html = "";
+    tweets.forEach(renderTweets);
+  } else {
+    html = "";
+    for (let i = 0; i < tweets.length; i++) {
+      if (tweets[i].name === filter) {
+        html += "<hr>"
+        html += `<img class="myAvatar" src="${tweets[i].avatar}" alt=""><b>${tweets[i].name}</b><br> ${tweets[i].message}<br><i>${tweets[i].tweetedAt}</i>`;
+      }
     }
   }
   tw.innerHTML = html;
