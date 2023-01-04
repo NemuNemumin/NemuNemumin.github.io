f927dc3f526de9825a1d8bc636f0d0a41f425821
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-01-05 03:06:58 +0900

    clickEventAdded

--- Ota_stampRally/main.js
+++ Ota_stampRally/main.js
@@ -1,6 +1,12 @@
+//modal control
 let modalOpen = document.querySelector(".open");
 let modal = document.querySelector(".input-modal");
 let modalClose = document.querySelector(".close");
-
 modalOpen.addEventListener("click",function (){modal.classList.add("open-modal")});
 modalClose.addEventListener("click",function (){modal.classList.remove("open-modal")});
+
+//get mouseXY
+function getMouseXY(){
+  console.log("クリックされました");
+}
+
