4068df3ba89d98857e5ab9494b80219694a2509d
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-01-06 02:54:22 +0900

    mouseXY view added

--- Ota_stampRally/main.js
+++ Ota_stampRally/main.js
@@ -5,12 +5,23 @@ let modalClose = document.querySelector(".close");
 modalOpen.addEventListener("click",function (){modal.classList.add("open-modal")});
 modalClose.addEventListener("click",function (){modal.classList.remove("open-modal")});
 
-//get mouseXY
+//global　declaration
+let mouseX = 0;
+let mouseY = 0;
+
+//get mouseXY (called when photo clicked)
 function getMouseXY(){
   //mouseXY -> x,y
-  var x = event.clientX;
-  var y = event.clientY;
-
-  document.querySelector("#mouseX").innerHTML = `X座標: ${x} `;
-  document.querySelector("#mouseY").innerHTML = `Y座標: ${y} `;
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+  //view mouseXY
+  document.querySelector("#mouseX").innerHTML = `X座標: ${mouseX} `;
+  document.querySelector("#mouseY").innerHTML = `Y座標: ${mouseY} `;
 }
+
+//comment & mouseXY -> googleSpreadSheet (called when submit clicked)
+function uploadGSS (){
+
+
+}
+
