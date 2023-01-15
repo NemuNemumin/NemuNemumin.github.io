355e3a0811728faf70b39111ed001c3f912193ca
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-01-15 12:20:55 +0900

    OK

--- Ota_stampRally/main.js
+++ Ota_stampRally/main.js
@@ -8,23 +8,23 @@ modalOpen.addEventListener("click",function (){modal.classList.add("open-modal")
 modalClose.addEventListener("click",function (){modal.classList.remove("open-modal")});
 
 //global　declaration
-let mouseX = 0;
-let mouseY = 0;
+let windowMouseX = 0;
+let windowMouseY = 0;
 
 //get mouseXY (called when photo clicked)
 function getMouseXY(){
   //mouseXY -> x,y
-  mouseX = event.clientX;
-  mouseY = event.clientY;
+  windowMouseX = event.clientX;
+  windowMouseY = event.clientY;
   //view mouseXY
-  document.querySelector("#mouseX").innerHTML = `X座標: ${mouseX} `;
-  document.querySelector("#mouseY").innerHTML = `Y座標: ${mouseY} `;
+  document.querySelector("#mouseX").innerHTML = `X座標: ${windowMouseX} `;
+  document.querySelector("#mouseY").innerHTML = `Y座標: ${windowMouseY} `;
 }
 
 //comment & mouseXY -> googleSpreadSheet (called when submit clicked)
 function uploadGSS (){
   const userText = document.getElementById("userText");
-  const userTextv = userText.value;
+  const userTextValue = userText.value;
 
   new Vue({
     el: '#vue',
@@ -36,9 +36,9 @@ function uploadGSS (){
     methods: {
       postData: function(){
         var params = new URLSearchParams();
-        params.append('mouseX', `${mouseX}`);
-        params.append('mouseY', `${mouseY}`);
-        params.append('text', `${userTextv}`);
+        params.append('mouseX', `${windowMouseX}`);
+        params.append('mouseY', `${windowMouseY}`);
+        params.append('text', `${userTextValue}`);
         axios.post('https://script.google.com/macros/s/AKfycbwXe4AUH0aF0vi4e8TdwVx1uEdC2ezcI2cyn95TL0OhEhjMaFL-gfJLaTTSKeKEk_ed/exec', params)
           .then( ( res ) => {
             console.log(res.data);
@@ -51,8 +51,4 @@ function uploadGSS (){
   });
   modal.classList.remove("open-modal");
   userText.value = "";
-}
-
-function addOpenModal(){
-  modal.classList.add("open-modal");
 }
