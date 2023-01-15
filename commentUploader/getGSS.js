497a8c6da7e999f8636587a66997ca06ae77c521
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-01-15 13:36:40 +0900

    comment position algorithm fixed

--- commentUploader/getGSS.js
+++ commentUploader/getGSS.js
@@ -1,19 +1,32 @@
 const mouseX = [];
 const mouseY = [];
 const text = [];
+const viewX = [];
+const viewY = [];
+
 let body = "";
 
-fetch("https://script.google.com/macros/s/AKfycbz5mctsHkMlV3etE2ZSjJqDWcpPlFqIkYVt7Q8nKR1rKmDaU_N6x2WueAf5_6-Ew1mq/exec")
-  .then(function (data){
-    return data.json();
-  })
-    .then(function (json){
-      for (let i = 0; i < json.length; i++) {
-        mouseX[i] = json[i].mouseX;
-        mouseY[i] = json[i].mouseY;
-        text[i] = json[i].text;
-        body += `<p class="text" style="top: ${mouseY[i]}px; left:${mouseX[i]}px">${text[i]}</p>`
-      }
-      $("#tags").append(body);
-    })
+function render_comment() {
+
+  fetch("https://script.google.com/macros/s/AKfycbz5mctsHkMlV3etE2ZSjJqDWcpPlFqIkYVt7Q8nKR1rKmDaU_N6x2WueAf5_6-Ew1mq/exec")
+      .then(function (data) {
+        return data.json();
+      })
+      .then(function (json) {
+        for (let i = 0; i < json.length; i++) {
+          mouseX[i] = json[i].mouseX;
+          mouseY[i] = json[i].mouseY;
+          text[i] = json[i].text;
+
+          viewX[i] = mouseX[i] * window.document.getElementById("mainMap").clientWidth;
+          viewY[i] = mouseY[i] * window.document.getElementById("mainMap").clientHeight;
+
+          body += `<p class="text" style="top: ${viewY[i]}px; left:${viewX[i]}px">${text[i]}</p>`
+        }
+        $("#tags").append(body);
+      })
+}
+
+render_comment()
+// window.onresize = render_comment;
 
--- commentUploader/main.js
+++ commentUploader/main.js
@@ -10,12 +10,23 @@ modalClose.addEventListener("click",function (){modal.classList.remove("open-mod
 //global　declaration
 let windowMouseX = 0;
 let windowMouseY = 0;
+let viewMouseX   = 0;
+let viewMouseY   = 0;
+let photoWidth = 0;
+let photoHeight = 0;
 
 //get mouseXY (called when photo clicked)
 function getMouseXY(){
   //mouseXY -> x,y
   windowMouseX = event.clientX;
   windowMouseY = event.clientY;
+
+  photoWidth  = window.document.getElementById("mainMap").clientWidth;
+  photoHeight = window.document.getElementById("mainMap").clientHeight;
+
+  viewMouseX = windowMouseX / photoWidth;
+  viewMouseY = windowMouseY / photoHeight;
+
   //view mouseXY
   document.querySelector("#mouseX").innerHTML = `X座標: ${windowMouseX} `;
   document.querySelector("#mouseY").innerHTML = `Y座標: ${windowMouseY} `;
@@ -36,8 +47,8 @@ function uploadGSS (){
     methods: {
       postData: function(){
         var params = new URLSearchParams();
-        params.append('mouseX', `${windowMouseX}`);
-        params.append('mouseY', `${windowMouseY}`);
+        params.append('mouseX', `${viewMouseX}`);
+        params.append('mouseY', `${viewMouseY}`);
         params.append('text', `${userTextValue}`);
         axios.post('https://script.google.com/macros/s/AKfycbwXe4AUH0aF0vi4e8TdwVx1uEdC2ezcI2cyn95TL0OhEhjMaFL-gfJLaTTSKeKEk_ed/exec', params)
           .then( ( res ) => {
