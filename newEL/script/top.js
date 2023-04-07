a353a8b2f5ce9d1e9f5365ff5b48b7cc063c800e
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-04-08 00:37:15 +0900

    カレンダー切り替え色変化機能追加

--- newEL/script/top.js
+++ newEL/script/top.js
@@ -37,6 +37,17 @@ function isDisplayCalendar03(){
   document.getElementById("sbh6").style.display = "none";
   document.getElementById("sbh7").style.display = "none";
   document.getElementById("sbh3").style.display = "block";
+
+  document.getElementById("3rd").style.color = "#FFFFFF";
+  document.getElementById("3rd").style.backgroundColor = "#2D4B70";
+  document.getElementById("4th").style.color = "";
+  document.getElementById("4th").style.backgroundColor = "";
+  document.getElementById("5th").style.color = "";
+  document.getElementById("5th").style.backgroundColor = "";
+  document.getElementById("6th").style.color = "";
+  document.getElementById("6th").style.backgroundColor = "";
+  document.getElementById("7th").style.color = "";
+  document.getElementById("7th").style.backgroundColor = "";
 }
 function isDisplayCalendar04(){
   document.getElementById("sbh3").style.display = "none";
@@ -44,6 +55,17 @@ function isDisplayCalendar04(){
   document.getElementById("sbh6").style.display = "none";
   document.getElementById("sbh7").style.display = "none";
   document.getElementById("sbh4").style.display = "block";
+
+  document.getElementById("4th").style.color = "#FFFFFF";
+  document.getElementById("4th").style.backgroundColor = "#2D4B70";
+  document.getElementById("3rd").style.color = "";
+  document.getElementById("3rd").style.backgroundColor = "";
+  document.getElementById("5th").style.color = "";
+  document.getElementById("5th").style.backgroundColor = "";
+  document.getElementById("6th").style.color = "";
+  document.getElementById("6th").style.backgroundColor = "";
+  document.getElementById("7th").style.color = "";
+  document.getElementById("7rd").style.backgroundColor = "";
 }
 function isDisplayCalendar05(){
   document.getElementById("sbh3").style.display = "none";
@@ -51,6 +73,17 @@ function isDisplayCalendar05(){
   document.getElementById("sbh6").style.display = "none";
   document.getElementById("sbh7").style.display = "none";
   document.getElementById("sbh5").style.display = "block";
+
+  document.getElementById("5th").style.color = "#FFFFFF";
+  document.getElementById("5th").style.backgroundColor = "#2D4B70";
+  document.getElementById("3rd").style.color = "";
+  document.getElementById("3rd").style.backgroundColor = "";
+  document.getElementById("4th").style.color = "";
+  document.getElementById("4th").style.backgroundColor = "";
+  document.getElementById("6th").style.color = "";
+  document.getElementById("6th").style.backgroundColor = "";
+  document.getElementById("7th").style.color = "";
+  document.getElementById("7th").style.backgroundColor = "";
 }
 function isDisplayCalendar06(){
   document.getElementById("sbh3").style.display = "none";
@@ -58,6 +91,17 @@ function isDisplayCalendar06(){
   document.getElementById("sbh5").style.display = "none";
   document.getElementById("sbh7").style.display = "none";
   document.getElementById("sbh6").style.display = "block";
+
+  document.getElementById("6th").style.color = "#FFFFFF";
+  document.getElementById("6th").style.backgroundColor = "#2D4B70";
+  document.getElementById("3rd").style.color = "";
+  document.getElementById("3rd").style.backgroundColor = "";
+  document.getElementById("4th").style.color = "";
+  document.getElementById("4th").style.backgroundColor = "";
+  document.getElementById("5th").style.color = "";
+  document.getElementById("5th").style.backgroundColor = "";
+  document.getElementById("7th").style.color = "";
+  document.getElementById("7th").style.backgroundColor = "";
 }
 function isDisplayCalendar07(){
   document.getElementById("sbh3").style.display = "none";
@@ -65,4 +109,15 @@ function isDisplayCalendar07(){
   document.getElementById("sbh5").style.display = "none";
   document.getElementById("sbh6").style.display = "none";
   document.getElementById("sbh7").style.display = "block";
+
+  document.getElementById("7th").style.color = "#FFFFFF";
+  document.getElementById("7th").style.backgroundColor = "#2D4B70";
+  document.getElementById("3rd").style.color = "";
+  document.getElementById("3rd").style.backgroundColor = "";
+  document.getElementById("4th").style.color = "";
+  document.getElementById("4th").style.backgroundColor = "";
+  document.getElementById("5th").style.color = "";
+  document.getElementById("5th").style.backgroundColor = "";
+  document.getElementById("6th").style.color = "";
+  document.getElementById("6th").style.backgroundColor = "";
 }
