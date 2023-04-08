db34613e19b319fc05b3d14540fca5215db70a1a
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-04-09 00:27:58 +0900

    カレンダー　デザイン変更

--- newEL/script/top.js
+++ newEL/script/top.js
@@ -36,21 +36,17 @@ function defaultScroll(month) {
   document.getElementById('calenderList').scrollLeft = flag * 225 + 140;
 }
 
-
-
 function isDisplayCalender(month){
   let flag = Number(month);
-
-
   for (let i = 0; i < 13; i++){
     if (i === flag) {
-      document.getElementById(`sbh${flag}`).style.display = "block";
-      document.getElementById(`menu${flag}`).style.color = "#FFFFFF";
-      document.getElementById(`menu${flag}`).style.backgroundColor = "#2D4B70";
+      document.getElementById(`sbh${flag}`).style.display = "block";　/*下のそれぞれの月の内容を表示させます。 i === flag つまり選択された月の数字の時だけ実行されます*/
+      document.getElementById(`menu${flag}`).style.color = "#FFFFFF"; /*選択された月の文字の色を白にします*/
+      document.getElementById(`menu${flag}`).style.backgroundColor = "#2D4B70"; /*選択された月の背景を変更します*/
       continue
     }
-      document.getElementById(`sbh${i}`).style.display = "none";
-      document.getElementById(`menu${i}`).style.backgroundColor = "";
-      document.getElementById(`menu${i}`).style.color = "";
+      document.getElementById(`sbh${i}`).style.display = "none"; /*選択されなかった月の内容を非表示にします*/
+      document.getElementById(`menu${i}`).style.backgroundColor = "";　/*選択されなかった月の文字をデフォルトに戻します*/
+      document.getElementById(`menu${i}`).style.color = ""; /*選択されなかった月の背景をデフォルトに戻します*/
     }
 }
