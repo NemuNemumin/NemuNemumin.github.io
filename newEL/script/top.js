d99f32389140bddb3229ba5d32ef503b3e181e76
AkitoNagashima <m0a20202af@edu.teu.ac.jp>
2023-04-09 04:30:13 +0900

    メモを追加しました。

--- newEL/script/top.js
+++ newEL/script/top.js
@@ -31,11 +31,13 @@ document.open();
 $(document).ready(function () {$('#calender-btn').dragScroll();});//適用するクラスやIDを入力してください
 document.close();
 
+/*bodyが読み込まれたとき(=ページが開いた時)に実行される関数です。html側の数字入力でデフォルトのスクロール位置が設定されます。*/
 function defaultScroll(month) {
   let flag = Number(month);
   document.getElementById('calenderList').scrollLeft = flag * 225 + 140;
 }
 
+/*カレンダーの選択ボタンを押したときに実行される関数です。*/
 function isDisplayCalender(month){
   let flag = Number(month);
   for (let i = 0; i < 13; i++){
@@ -43,7 +45,7 @@ function isDisplayCalender(month){
       document.getElementById(`sbh${flag}`).style.display = "block";　/*下のそれぞれの月の内容を表示させます。 i === flag つまり選択された月の数字の時だけ実行されます*/
       document.getElementById(`menu${flag}`).style.color = "#FFFFFF"; /*選択された月の文字の色を白にします*/
       document.getElementById(`menu${flag}`).style.backgroundColor = "#2D4B70"; /*選択された月の背景を変更します*/
-      continue
+      continue/*以降の処理をスキップし、次のforループに移ります。それによって、せっかく表示したのに非表示にする処理が走る...ということを防ぐことができます。*/
     }
       document.getElementById(`sbh${i}`).style.display = "none"; /*選択されなかった月の内容を非表示にします*/
       document.getElementById(`menu${i}`).style.backgroundColor = "";　/*選択されなかった月の文字をデフォルトに戻します*/
